7002dd4770dd1748fb9b4bb350565af62d05586c
General Coding <10633257+general-coding@users.noreply.github.com>
2024-02-10 19:15:55 +0530

    Update E191_NumberOf1Bits.java

--- src/algorithms/easy/E191_NumberOf1Bits.java
+++ src/algorithms/easy/E191_NumberOf1Bits.java
@@ -6,9 +6,9 @@ package algorithms.easy;
  *         https://leetcode.com/problems/number-of-1-bits/
  * 
  *         Write a function that takes an unsigned integer and returns the
- *         number of ’1' bits it has (also known as the Hamming weight).
+ *         number of â€™1' bits it has (also known as the Hamming weight).
  * 
- *         For example, the 32-bit integer ’11' has binary representation
+ *         For example, the 32-bit integer â€™11' has binary representation
  *         00000000000000000000000000001011, so the function should return 3.
  *
  */
@@ -16,17 +16,12 @@ package algorithms.easy;
 public class E191_NumberOf1Bits {
 
 	public int hammingWeight(int n) {
-		String s = Integer.toBinaryString(n);
-		System.out.println(s);
-		int oneCount = 0;
-
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '1') {
-				oneCount++;
-			}
-		}
-
-		return oneCount;
+	        int res = 0;
+	        while(n != 0){
+	            res = res + (n & 1);
+	            n = n >>> 1;
+	        }
+		return res;
 	}
 
 	public static void main(String[] args) {
