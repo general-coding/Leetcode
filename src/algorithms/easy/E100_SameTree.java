46d3977afd3b0c204a09fecae872b0c8a380c132
General Coding <10633257+general-coding@users.noreply.github.com>
2024-02-10 18:14:34 +0530

    Update E100_SameTree.java

--- src/algorithms/easy/E100_SameTree.java
+++ src/algorithms/easy/E100_SameTree.java
@@ -26,15 +26,22 @@ public class E100_SameTree {
 	}
 
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if (p == null && q == null) {
-			return true;
-		}
-
-		if ((p != null && q == null) || (p == null & q != null) || (p.val != q.val)) {
-			return false;
-		}
-
-		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	        if(p == null && q == null)
+	        {
+	            return true;
+	        }
+	
+	        if(p == null || q == null)
+	        {
+	            return false;
+	        }
+	
+	        if(p.val != q.val)
+	        {
+	            return false;
+	        }
+	       
+	        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 	}
 
 	public static void main(String[] args) {
