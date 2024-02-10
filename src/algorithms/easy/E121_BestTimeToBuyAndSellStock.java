acfd11390f7deb23110b6d8ea4d72644c1cd80d9
General Coding <10633257+general-coding@users.noreply.github.com>
2024-02-10 18:36:51 +0530

    Update E121_BestTimeToBuyAndSellStock.java

--- src/algorithms/easy/E121_BestTimeToBuyAndSellStock.java
+++ src/algorithms/easy/E121_BestTimeToBuyAndSellStock.java
@@ -16,28 +16,20 @@ package algorithms.easy;
 
 public class E121_BestTimeToBuyAndSellStock {
 	public int maxProfit(int[] prices) {
-		if (prices.length <= 1) {
-			return 0;
-		}
-
-		int min = prices[0];
-		int max = prices[1] - min;
-
-		for (int i = 2; i < prices.length; i++) {
-			if (min > prices[i - 1]) {
-				min = prices[i - 1];
-			}
-
-			if (max < prices[i] - min) {
-				max = prices[i] - min;
-			}
-		}
-
-		if (max < 0) {
-			return 0;
-		} else {
-			return max;
-		}
+	        int maxProfit = 0;
+	
+	        int entryPrice = int.MaxValue;
+	
+	        foreach (int p in prices)
+	        {
+	            if (p < entryPrice)
+	            {
+	                entryPrice = p;
+	            }
+	            maxProfit = Math.Max(maxProfit, p - entryPrice);
+	        }
+	
+	        return maxProfit;
 	}
 
 	public static void main(String[] args) {
