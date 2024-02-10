b0ef7635273cbcc8cb6b67228e6edee9ed0f7890
General Coding <10633257+general-coding@users.noreply.github.com>
2024-02-10 16:10:23 +0530

    Update E083_RemoveDuplicatesFromSortedList.java

--- src/algorithms/easy/E083_RemoveDuplicatesFromSortedList.java
+++ src/algorithms/easy/E083_RemoveDuplicatesFromSortedList.java
@@ -25,24 +25,21 @@ public class E083_RemoveDuplicatesFromSortedList {
 	}
 
 	public ListNode deleteDuplicates(ListNode head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-
-		ListNode prev = head;
-		ListNode curr = head.next;
-
-		while (curr != null) {
-			if (curr.val == prev.val) {
-				prev.next = curr.next;
-				curr = curr.next;
-			} else {
-				prev = curr;
-				curr = curr.next;
-			}
-		}
-		return head;
+	        ListNode it = head;
+	        while(it != null && it.next != null){
+	            while(it.next != null && it.val == it.next.val){
+	                deleteDuplicate(ref it);
+	            }
+	            it = it.next;
+	        }
+	        return head;
 	}
+
+	    private void deleteDuplicate(ref ListNode node){
+	        ListNode next = node.next.next;
+	        node.next.next = null;
+	        node.next = next;
+	    }
 
 	public static void main(String[] args) {
 		System.out.println(new E083_RemoveDuplicatesFromSortedList().deleteDuplicates(null));
